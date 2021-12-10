e359ea4ebcd8fdc1bb7dda34ef697a519b805c3e
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-10 14:57:49 +0100

    For Loop set up done with comments

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,13 @@
 // Ciclo for
 
 for (let i = 1; i < 101; i++) {
-    console.log(i)
+    if (i % 5 === 0 && i % 3 === 0){
+        // Stampa FizzBuzz
+
+    } else if (i % 5 === 0){
+        // Stampa Buzz
+
+    } else (i % 3 === 0){
+        // Stampa Fizz
+    }
 }
