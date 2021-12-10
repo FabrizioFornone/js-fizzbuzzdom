96e2a2c6058fd6da04a2429505459332db6f3f83
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-10 15:45:38 +0100

    Exercise Done: little fixes

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const output1Html = document.querySelector(".container-js");
 
 // For Loop
 
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i <= 100; i++) {
   if (i % 5 === 0 && i % 3 === 0) {
     output1Html.innerHTML += `<div class="box box-red"><div>FizzBuzz</div></div>`;
   } else if (i % 5 === 0) {
