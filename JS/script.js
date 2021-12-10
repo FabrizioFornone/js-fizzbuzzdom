3ba3914d82a5f40684fb05424a7608a425f1dfa1
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-10 15:23:10 +0100

    Exercise almost done

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,17 @@
-// Ciclo for
+//Declaration Const
+
+const output1Html = document.querySelector(".container-js");
+
+// For Loop
 
 for (let i = 1; i < 101; i++) {
-    if (i % 5 === 0 && i % 3 === 0){
-        // Stampa FizzBuzz
-
-    } else if (i % 5 === 0){
-        // Stampa Buzz
-
-    } else (i % 3 === 0){
-        // Stampa Fizz
-    }
+  if (i % 5 === 0 && i % 3 === 0) {
+    output1Html.innerHTML += `<div class="box box-red">FizzBuzz</div>`;
+  } else if (i % 5 === 0) {
+    output1Html.innerHTML += `<div class="box box-yellow">Buzz</div>`;
+  } else if (i % 3 === 0) {
+    output1Html.innerHTML += `<div class="box box-green">Fizz</div>`;
+  } else {
+    output1Html.innerHTML += `<div class="box box-blue">${i}</div>`;
+  }
 }
